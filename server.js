4cf4f8bb63e74ab57fdb38a4b236a754ff88ba06
Steve Wichelecki <swichelecki@wciu.com>
2019-08-01 10:02:20 -0500

    attempt

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const jsonServer = require('json-server');
 
 //const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
 //const port = process.env.PORT || 3000;
 
