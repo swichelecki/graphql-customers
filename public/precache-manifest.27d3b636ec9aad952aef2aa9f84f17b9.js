7ea957680e5f8ed20cd9744035c328b0b545a57a
Steve Wichelecki <swichelecki@wciu.com>
2019-08-01 08:29:09 -0500

    second commit

---
+++ public/precache-manifest.27d3b636ec9aad952aef2aa9f84f17b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e3f6841ae449674a1d61729a0565c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ad54c9942517c47b4f4",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "700f4494765ca53446da",
+    "url": "/static/js/2.78cae20c.chunk.js"
+  },
+  {
+    "revision": "5ad54c9942517c47b4f4",
+    "url": "/static/js/main.7f3a2891.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const cors = require('cors');
 const schema = require('./schema.js');
+const path = require('path');
 
 const app = express();
+
+app.use(cors());
 
 app.use('/graphql', expressGraphQL({
     schema: schema,
     graphiql: true
 }));
 
-app.listen(4000, () => {
-    console.log('Server is running on port 4000.');
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 })
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
